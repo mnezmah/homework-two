1fbccc1b9ad2dd8d9de25210fd818d1dd5ed57f1
mnezmah <mario.nezmah@gmail.com>
2019-06-08 17:41:36 +0200

    transfrom resolved

--- src/2-transform.spec.js
+++ src/2-transform.spec.js
@@ -18,7 +18,7 @@ describe("Assignment 2 - transform", function () {
     ])
     assert.deepStrictEqual(grouping, {})
   })
-  it.only("only contains non-empty groups", function() {
+  it("only contains non-empty groups", function() {
     const groupAdultsByAgeRange = require("./transform").groupAdultsByAgeRange
     const grouping = groupAdultsByAgeRange([{name: "Anna", age: 31}, {name: "John", age: 32}, {name: "Hank", age: 60}])
     const result = { 
--- src/transform.js
+++ src/transform.js
@@ -2,7 +2,7 @@
 
 
 function groupAdultsByAgeRange(people) {
-  const groups = {}
+  let groups = {}
   let youngsters = []
   let twentyThirty = []
   let thirtyFourty = []
@@ -12,56 +12,32 @@ function groupAdultsByAgeRange(people) {
     return person.age >= 18
   }))
 
-// const youngsters = adults.filter(adult => adult.age < 20)
-// const 
+  // const youngsters = adults.filter(adult => adult.age < 20)
+  // const 
 
 
- adults.map((adult) => {
-    
-    if (adult.age >= 18 && adult.age < 20) {
-      youngsters.push(adult)
-    }
-    if (adult.age >= 20 && adult.age < 31) {
-      twentyThirty.push(adut)
-    }
-    if (adult.age >= 31 && adult.age < 41) {
-      thirtyFourty.push(adult)
-    }
-    if (adult.age >= 41 && adult.age < 51) {
-      fourtyFifty.push(adult)
-    }
-    else {
-      older.push(adult)
-    }
+  adults.map((adult) => {
+    if (adult.age >= 18 && adult.age <= 20) { youngsters.push(adult) }
+    if (adult.age >= 21 && adult.age <= 30) { twentyThirty.push(adult) }
+    if (adult.age >= 31 && adult.age <= 40) { thirtyFourty.push(adult) }
+    if (adult.age >= 41 && adult.age <= 50) { fourtyFifty.push(adult) }
+    if (adult.age > 50) { older.push(adult) }
   })
 
   // console.log('my youngsters are', youngsters)
 
-  
-  
+  // console.log('my 30-40 are', thirtyFourty)
 
-  if (youngsters.length >= 1) {
-    groups["20 and younger"] = youngsters
 
-    if (twentyThirty.length >= 1) {
-      groups["21-30"] = twentyThirty
-    }
-    if (thirtyFourty.length >= 1) {
-      groups["31-40"] = thirtyFourty.map((single) => {
-        return single
-      })
-    }  console.log('my 30-40 are', thirtyFourty)
-    if (fourtyFifty.length >= 1) {
-      groups["41-50"] = fourtyFifty
-    }
-    if (older.length >= 1) {
-      groups["51 and older"] = older
-    } 
-  }
-  
-  
-  return groups
-  
+  if (youngsters.length !== 0) { groups["20 and younger"] = youngsters }
+  if (twentyThirty.length !== 0) { groups["21-30"] = twentyThirty }
+  if (thirtyFourty.length !== 0) { groups["31-40"] = thirtyFourty }
+  if (fourtyFifty.length !== 0) { groups["41-50"] = fourtyFifty }
+  if (older.length !== 0) { groups["51 and older"] = older }
+
+  console.log('evo moje omladine!!!!!!!!!!!', youngsters)
+return groups
+
 }
 
 
